93c60eb13a382279944670a8931fae134e9a003c
xfufnx <xfufnx@gmail.com>
2023-02-19 21:48:46 +0600

    minor fix

--- src/main/java/com/fufn/task/controller/CodeController.java
+++ src/main/java/com/fufn/task/controller/CodeController.java
@@ -3,6 +3,8 @@ package com.fufn.task.controller;
 import com.fufn.task.entity.Code;
 import com.fufn.task.service.CodeService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CodeController {
 
     private final CodeService codeService;
+    private final Logger logger = LogManager.getLogger(getClass());
 
     @GetMapping(value = "/codes")
     public Code getCode(){
+        logger.info("GET request to get new unique code");
         return codeService.getCode();
     }
 
